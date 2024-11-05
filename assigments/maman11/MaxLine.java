5d98e6edd64c901295afdc7b648dec2a247b3d28
Yocheved Philip <yocheved.filip@gmail.com>
2024-11-05 10:55:16 +0200

    Add Coins.java

---
+++ assigments/maman11/MaxLine.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/*
+ * A class that get 3 points coordinats, and return the longest
+ */
+public class MaxLine{ 
+
+    public static void main (String [] args) {
+
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter first point coordinates:");
+        int x1 = scan.nextInt();
+        int y1 = scan.nextInt();
+        System.out.println ("Enter second point coordinates:");
+        int x2 = scan.nextInt();
+        int y2 = scan.nextInt();
+        System.out.println ("Enter third point coordinates:");
+        int x3 = scan.nextInt();
+        int y3 = scan.nextInt();
+        //...
+    } // end of method main
+} //end of class MaxLine
